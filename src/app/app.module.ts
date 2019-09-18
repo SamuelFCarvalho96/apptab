879a69caee48e675270b0278e32f0d0b46d07887
SAMUEL FERREIRA DE CARVALHO <samuel.fcarvalho1@senacsp.edu.br>
2019-09-18 20:02:48 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { firebaseconfig } from './../firebase-config';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -9,10 +10,19 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-@NgModule({
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+@ NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(),
+            AppRoutingModule,
+            AngularFireModule.initializeApp(firebaseconfig),
+            AngularFireAuthModule,
+            AngularFireDatabaseModule
+          ],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/usuarios/perfil/perfil.page.ts
@@ -0,0 +1,26 @@
+import { Router } from '@angular/router';
+import { UsuariosService } from './../shared/usuarios.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.page.html',
+  styleUrls: ['./perfil.page.scss'],
+})
+export class PerfilPage implements OnInit {
+user: any = {};
+  constructor(private usuariosService: UsuariosService,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.user = this.usuariosService.getDadosUsuario();
+  }
+
+  sair() {
+    this.usuariosService.logout()
+    .then(() => {
+      this.router.navigate(['/login']);
+    })
+  }
+
+}
